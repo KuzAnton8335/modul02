646dda8673cca20bcc2eb0564a8e09bf59b51550
KuzAnton8335 <ant2783@gmail.com>
2023-11-07 12:17:02 +0300

    names

--- lesson01/js/main.js
+++ lesson01/js/main.js
@@ -1,24 +1,24 @@
-const Items = document.querySelector('.items');
-const Item = document.querySelector('.item')
-const ItemOne = document.querySelector('.item_one');
-const ItemTwo = document.querySelector('item_two');
-const ItemThree = document.querySelector('.item_three');
-const ItemFour = document.querySelector('.item_four');
-const ItemFive = document.querySelector('.item_five');
-const ItemSix = document.querySelector('.item_six');
-const PropsList = document.querySelectorAll('.props__list');
-const PropsItem = document.querySelectorAll('.props__item');
+const items = document.querySelector('.items');
+const item = document.querySelector('.item')
+const itemOne = document.querySelector('.item_one');
+const itemTwo = document.querySelector('item_two');
+const itemThree = document.querySelector('.item_three');
+const itemFour = document.querySelector('.item_four');
+const itemFive = document.querySelector('.item_five');
+const itemSix = document.querySelector('.item_six');
+const propsList = document.querySelectorAll('.props__list');
+const propsItem = document.querySelectorAll('.props__item');
 const tagNameLi = document.getElementsByTagName('li');
 const tagNameOl = document.getElementsByTagName('Ol');
-const Ads = document.querySelector('.ads');
-const parent = Ads.parentNode;
+const ads = document.querySelector('.ads');
+const parent = ads.parentNode;
 
 
-Items.append(ItemThree, ItemFour, ItemFive, ItemSix);
-PropsList[0].replaceWith(PropsItem[0], PropsItem[1], PropsItem[2], PropsItem[14], PropsItem[3], PropsItem[4])
-PropsList[2].prepend(PropsItem[43], PropsItem[44]);
-PropsList[2].replaceWith(PropsItem[11], PropsItem[12], PropsItem[13], PropsItem[15], PropsItem[16], PropsItem[17],
-	PropsItem[18], PropsItem[19], PropsItem[43], PropsItem[44]);
-PropsList[3].replaceWith(PropsItem[28], PropsItem[29], PropsItem[30], PropsItem[31], PropsItem[32], PropsItem[33], PropsItem[34])
-PropsList[4].replaceWith(PropsItem[20], PropsItem[21], PropsItem[22], PropsItem[23], PropsItem[24], PropsItem[25], PropsItem[26], PropsItem[27])
-parent.removeChild(Ads);
+items.append(itemThree, itemFour, itemFive, itemSix);
+propsList[0].replaceWith(propsItem[0], propsItem[1], propsItem[2], propsItem[14], propsItem[3], propsItem[4])
+propsList[2].prepend(propsItem[43], propsItem[44]);
+propsList[2].replaceWith(propsItem[11], propsItem[12], propsItem[13], propsItem[15], propsItem[16], propsItem[17],
+	propsItem[18], propsItem[19], propsItem[43], propsItem[44]);
+propsList[3].replaceWith(propsItem[28], propsItem[29], propsItem[30], propsItem[31], propsItem[32], propsItem[33], propsItem[34])
+propsList[4].replaceWith(propsItem[20], propsItem[21], propsItem[22], propsItem[23], propsItem[24], propsItem[25], propsItem[26], propsItem[27])
+parent.removeChild(ads);
